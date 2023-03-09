695dbfbb7fa296ba51ec0eb3a73a44a1a92e54e1
Angelo Bordin <angelombordin@gmail.com>
2023-03-09 14:49:40 -0300

    create style for task completed

--- src/components/Activities_List/Activity/Activity.tsx
+++ src/components/Activities_List/Activity/Activity.tsx
@@ -8,9 +8,13 @@ interface IActivityProp extends ITask {
 export default function Activity(task: IActivityProp) {
     const selectTaskFunction = task.selectTask;
     return (
-        <li className={`${style.item} ${task.selected ? style.itemSelecionado : style.item}`} onClick={() => selectTaskFunction(task)}>
+        <li className={`${style.item} 
+                        ${task.selected ? style.itemSelecionado : style.item}
+                        ${task.completed ? style.itemCompletado : ''} `} 
+            onClick={() => !task.completed && selectTaskFunction(task)}>
             <h3>{task.name}</h3>
             <span>{task.time}</span>
+            {task.completed && <span className={style.concluido}></span>}
         </li>
     )
 }
